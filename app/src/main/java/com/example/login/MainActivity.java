15a7b07bde183e827f66f2918a7473f5749aa551
murmusandeep <aaryapooh@gmail.com>
2020-04-02 19:03:18 +0530

    First Commit

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -23,16 +23,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnLogin = findViewById(R.id.btn_login);
 
         btnLogin.setOnClickListener(this);
+        EditText editText = (EditText) findViewById(R.id.input_password);
+        String value = editText.getText().toString();
 
     }
 
 
     @Override
     public void onClick(View v) {
+        EditText editText = (EditText) findViewById(R.id.input_password);
+        String value = editText.getText().toString();
+        validatePassword(value);
         switch (v.getId())
         {
-            case R.id.btn_login:Toast.makeText(this, "Login Button", Toast.LENGTH_LONG).show();
+            case R.id.btn_login:
+                //Toast.makeText(this,value, Toast.LENGTH_LONG).show();
             break;
+        }
+    }
+
+    private void validatePassword(String stringEditText) {
+        int smallChar = 0,bigChar = 0,number = 0,specialChar = 0;
+        for( int i = 0 ;i<stringEditText.length(); i++)
+        {
+            if(stringEditText.charAt(i) >= 'a' && stringEditText.charAt(i) <= 'z')
+            {
+                smallChar++;
+            }
+            if(stringEditText.charAt(i) >= 'A' && stringEditText.charAt(i) <= 'Z')
+            {
+                bigChar++;
+            }
+            if(Character.isDigit(stringEditText.charAt(i)))
+            {
+                number++;
+            }
+            else
+            {
+                specialChar++;
+            }
+        }
+
+        if(smallChar == 0 || bigChar == 0 || number == 0 || specialChar == 0)
+        {
+            Toast.makeText(MainActivity.this, "Password Not Strong", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this, "Password Strong", Toast.LENGTH_SHORT).show();
         }
     }
 }
